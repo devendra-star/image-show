5bf1996d367d0e7ec8f2199796d024c6dc66e42e
yadavdevendra <devendrayadav2022aug@cedcommerce.com>
2023-03-23 13:56:18 +0530

    first commit

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,14 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setProducts } from "../redux/actions/productsActions";
 import ProductComponent from "./ProductComponent";
 
 
 const ProductPage = () => {
+  const [query, setQuery] = useState("ocean")
   const dispatch = useDispatch();
   const token = 'q7tLVIGkiL9IeH5KRvTNsL6DiHkRTJhWNnwr2YQwN9rCObVcljAUxaMI'
   const fetchProducts = () => {
-    fetch("https://api.pexels.com/v1/curated?page=2&per_page=40", {
+    fetch(`https://api.pexels.com/v1/search?page=2&per_page=40&query=${query}`, {
       headers: {
         Authorization: token
       }
@@ -27,11 +28,29 @@ const ProductPage = () => {
     fetchProducts();
   }, []);
 
+  function hendleClick(e) {
+    if (e.keyCode === 13) {
+      fetchProducts();
+    }
+
+  }
+
   // console.log("Products :", products);
   return (
-    <div className="ui grid container">
-      <ProductComponent />
-    </div>
+    <>
+      <div className="main">
+        <input type="text" height="100%" width="100%"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="serach image..."
+          onKeyDown={hendleClick}
+        />
+      </div>
+
+      <div className="ui grid container">
+        <ProductComponent />
+      </div>
+    </>
   );
 };
 
